5857071ed1ae52c79cfafebe9504620fbed82781
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-09-20 12:09:34 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { Card, Row, Col, Container } from "react-bootstrap";
 export default function App() {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
@@ -17,13 +18,28 @@ export default function App() {
 
   useEffect(() => {
     getProducts();
-  }, []);
-  console.log(products);
+  });
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container>
+      <Row>
+        {products.map((product) => (
+          <Col md={3} lg={4}>
+            <Card className="mb-3" style={{ height: "450px" }}>
+              <Card.Img
+                src={product.image}
+                alt="product"
+                width={200}
+                height={300}
+              />
+              <Card.Body>
+                <p>${product.price}</p>
+                <p>{product.title}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
+import "./bootstrap.min.css";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
